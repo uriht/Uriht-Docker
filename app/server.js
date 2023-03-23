e4a33fa7cf6b235c73eaa7ba201fc3fe3323ee9b
akauriht <akauriht@gmail>
2023-03-23 21:35:33 +0530

    Completed !!!

--- app/server.js
+++ app/server.js
@@ -30,7 +30,7 @@ let mongoUrlDocker = "mongodb://admin:password@mongodb";
 let mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
 
 // "user-account" in demo with docker. "my-db" in demo with docker-compose
-let databaseName = "my-db";
+let databaseName = "";
 
 app.post('/update-profile', function (req, res) {
   let userObj = req.body;
